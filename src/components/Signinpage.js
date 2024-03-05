2617b3ff1d83990c811bfb492a54a3b5988fb102
DaintyZawak <wakilatzakariyau@gmail.com>
2024-03-05 03:54:10 -0800

    updates

--- src/components/Signinpage.js
+++ src/components/Signinpage.js
@@ -1,30 +1,67 @@
+import { toHaveTextContent } from "@testing-library/jest-dom/matchers"
 import Button from "./Buttton"
 
 export default function Signin() {
+    const [formData, setFormData] = useState(
+        {
+            emailAddress: "", 
+            password: "",
+        }
+        
+        )
+
+        console.log(formData)
+    
+    function handleChange(event) {
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [event.target.name]: event.target.value
+            }
+        })
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault()
+
+        if(formData.password === formData.confirmPassword){
+           console.log("Successfully signed up")
+        } else {
+            console.log("Passwords do not match")
+        }
+
+        // submitToApi(formData)
+    }
+    
+
     return (
         <>
 
             <div className="onboardingPage">
-
-                <div className="welcomeTxt">
+            <div className="welcomeTxt">
                 <h1>Welcome Back to Convene!</h1>
                 <h2>Login</h2>
                 </div>
-        
-                <form>
-                    <label for='username'> Username/Email 
-                        <input type='text' placeholder='Username / Email' />
-                    </label>
 
-                    <label for='password'> Password
-                        <input type='password' placeholder='Password' />
-                    </label>
 
-                    <Button 
-                    text= "Login"
+                
 
-                    />
+                <form onSubmit={handleSubmit}>
                     
+                    
+
+                    <label htmlFor='emailAddress'> Email Address</label>
+                        <input type='email' placeholder='Email' 
+                        id="emailAddress" name="emailAddress" onChange={handleChange} value={formData.emailAddress} />
+                    
+                    <label htmlFor='password'> Password</label>
+                        <input type='password' placeholder='Password'
+                        id="password" name="password" onChange={handleChange} value={formData.password} />
+
+                    
+                    
+
+                   <Button text= "Login"/>
                 </form>
             </div>
 
--- src/components/Signuppage.js
+++ src/components/Signuppage.js
@@ -1,34 +1,182 @@
-import Button from "./Buttton"
+import { useState } from "react";
+import Button from "./Buttton";
 export default function Signup() {
-    return (
-        <>
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    emailAddress: "",
+    password: "",
+    confirmPassword: "",
+    comments: "",
+    agreement: true,
+    contact: "",
+    location: "",
+  });
 
-            <div className="onboardingPage">
-                <div class='welcomeTxt'>
-                <h1>Welcome to Convene!</h1>
-                <h2>Create account</h2>
+  console.log(formData);
 
-                <p>Already have an account? <a href=''>Sign in</a></p> 
-                </div>
+  function handleChange(event) {
+    const { name, value, type, checked } = event.target;
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [name]: type === "checkbox" ? checked : value,
+      };
+    });
+  }
 
-                <form>
-                    <label for='firstName'> First Name
-                        <input type='text' placeholder='First Name' />
-                    </label>
+  function handleSubmit(event)
+        {
+          event.preventDefault()
+          // submitToApi(formData)
+        }
 
-                    <label for='lastName'> Last Name
-                        <input type='text' placeholder='Last Name' />
-                    </label>
+  return (
+    <>
+      <div className="onboardingPage">
+        <div class="welcomeTxt">
+          <h1>Welcome to Convene!</h1>
+          <h2>Create account</h2>
 
-                    <label for='emailAddress'> Email Address
-                        <input type='email' placeholder='Email' />
-                    </label>
+          <p>
+            Already have an account? <a href="">Sign in</a>
+          </p>
+        </div>
+        
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="firstName"> First Name</label>
+          <input
+            type="text"
+            placeholder="First Name"
+            id="firstName"
+            name="firstName"
+            onChange={handleChange}
+            value={formData.firstName}
+          />
 
-                    <Button text= "Sign up"/>
-                    <p>I have read and agree to the <a href=''>Terms of Service</a></p>
-                </form>
-            </div>
+          <label htmlFor="lastName"> Last Name</label>
+          <input
+            type="text"
+            placeholder="Last Name"
+            id="lastName"
+            name="lastName"
+            onChange={handleChange}
+            value={formData.lastName}
+          />
 
-        </>
-    )
+          <label htmlFor="emailAddress"> Email Address</label>
+          <input
+            type="email"
+            placeholder="Email"
+            id="emailAddress"
+            name="emailAddress"
+            onChange={handleChange}
+            value={formData.emailAddress}
+          />
+
+          <label htmlFor="password"> Password</label>
+          <input
+            type="password"
+            placeholder="Password"
+            id="password"
+            name="password"
+            onChange={handleChange}
+            value={formData.password}
+          />
+
+          <label htmlFor="confirmPassword"> Password</label>
+          <input
+            type="password"
+            placeholder="confirmPassword"
+            id="confirmPassword"
+            name="confirmPassword"
+            onChange={handleChange}
+            value={formData.password}
+          />
+
+          <label htmlFor="agreement">
+            {" "}
+            I have read and agreed to the <a href="">Terms of Service</a>
+          </label>
+          <input
+            id="agreement"
+            type="checkbox"
+            onChange={handleChange}
+            name="agreement"
+            checked={formData.agreement}
+          />
+
+          <fieldset>
+            <legend>How did you find out about us?</legend>
+            <label htmlFor="instagram">Instagram</label>
+            <input
+              id="instagram"
+              name="contact"
+              value="instagram"
+              type="radio"
+              onChange={handleChange}
+              checked={formData.contact === "instagram"}
+            />
+
+            <label htmlFor="facebook">Facebook</label>
+            <input
+              id="facebook"
+              name="contact"
+              value="facebook"
+              type="radio"
+              onChange={handleChange}
+              checked={formData.contact === "instagram"}
+            />
+
+            <label htmlFor="newsletter">Newsletter</label>
+            <input
+              id="newsletter"
+              name="contact"
+              value="newsletter"
+              type="radio"
+              onChange={handleChange}
+              checked={formData.contact === "instagram"}
+            />
+
+            <label htmlFor="other">Other</label>
+            <input
+              id="other"
+              name="contact"
+              value="other"
+              type="radio"
+              onChange={handleChange}
+              checked={formData.contact === "instagram"}
+            />
+
+            <label htmlFor="location"> Where is your location? </label>
+            <select
+              id="location"
+              value={formData.location}
+              onChange={handleChange}
+              name="location"
+            >
+              <option value="">--- Choose ---</option>
+              <option value="lagos">Lagos</option>
+              <option value="ogun">Ogun</option>
+              <option value="oyo">Oyo</option>
+              <option value="ondo">Ondo</option>
+              <option value="ekiti">Ekiti</option>
+            </select>
+
+            <label htmlFor="comments">
+              <textarea
+                value={formData.comments}
+                placeholder="Any comments for us?"
+                id="comments"
+                onChange={handleChange}
+                name="comments"
+              />{" "}
+            </label>
+          </fieldset>
+
+          <Button text="Sign up" />
+        </form>
+      </div>
+    </>
+  );
 }
